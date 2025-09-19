e3cbba91152f241c7711b59ebca5362df6fe690d
Vova <103131693+vovkasWeb@users.noreply.github.com>
2025-09-19 17:45:11 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,53 @@
+import { category } from './obj.js'
+const btns = document.querySelectorAll('.category__btn-block .category__btn')
+const blockPartners = document.querySelector('.category__partners-block')
+let filter = 'slots'
+
+const element = element => {
+	return `
+    <div class="category__item-block">
+        <div class="category__img-item">
+            <img src="${element.img.url}" alt="${element.img.alt}" />
+        </div>
+        <div class="category__info-item">
+            <h4 class="category__title-item">${element.title}</h4>
+            <span class="category__text-item">
+               ${element.text}
+            </span>
+            <div class="category__btns-item">
+                <a class="category__btn-review" href="${element.review}">Review</a>
+                <a class="category__btn-play" href="${element.play}">Play</a>
+            </div>
+        </div>
+    </div>
+`
+}
+
+const viewElements = filter => {
+      blockPartners.innerHTML = ''
+    if (category[filter]){
+        btns.forEach(btn => {
+            btn.classList.remove('active')
+            if(btn.dataset.filter == filter){
+                btn.classList.add('active')
+            }
+        })
+        let arrElement = category[filter].map(el => element(el));
+        blockPartners.innerHTML = arrElement.join(""); 
+    }else{
+        blockPartners.innerHTML = 'No elements';
+    }
+    
+}
+btns.forEach(btn => {
+	btn.addEventListener('click', e => {
+		if (filter == e.currentTarget.dataset.filter) {
+			console.log(filter)
+			return
+		}
+		filter = e.currentTarget.dataset.filter
+		viewElements(filter)
+	})
+})
+
+viewElements(filter)
---
+++ js/obj.js
@@ -0,0 +1,192 @@
+export const category = {
+	slots: [
+		{
+			img: { url: './img/partners/caswino.png', alt: 'caswino' },
+			title: 'Caswino',
+			text: '600% UP TO 4800$ <br> +255 FREE SPINS',
+			btns: {
+				review: '',
+				play: '',
+			},
+		},
+		{
+			img: { url: './img/partners/caswino.png', alt: 'caswino' },
+			title: 'Caswino',
+			text: '600% UP TO 4800$ <br> +255 FREE SPINS',
+			btns: {
+				review: '',
+				play: '',
+			},
+		},
+		{
+			img: { url: './img/partners/caswino.png', alt: 'caswino' },
+			title: 'Caswino',
+			text: '600% UP TO 4800$ <br> +255 FREE SPINS',
+			btns: {
+				review: '',
+				play: '',
+			},
+		},
+		{
+			img: { url: './img/partners/caswino.png', alt: 'caswino' },
+			title: 'Caswino',
+			text: '600% UP TO 4800$ <br> +255 FREE SPINS',
+			btns: {
+				review: '',
+				play: '',
+			},
+		},
+	],
+	roulette: [
+		{
+			img: { url: './img/partners/caswino.png', alt: 'caswino' },
+			title: 'Caswino2',
+			text: '600% UP TO 4800$ <br> +255 FREE SPINS',
+			btns: {
+				review: '',
+				play: '',
+			},
+		},
+		{
+			img: { url: './img/partners/caswino.png', alt: 'caswino' },
+			title: 'Caswino2',
+			text: '600% UP TO 4800$ <br> +255 FREE SPINS',
+			btns: {
+				review: '',
+				play: '',
+			},
+		},
+		{
+			img: { url: './img/partners/caswino.png', alt: 'caswino' },
+			title: 'Caswino2',
+			text: '600% UP TO 4800$ <br> +255 FREE SPINS',
+			btns: {
+				review: '',
+				play: '',
+			},
+		},
+		{
+			img: { url: './img/partners/caswino.png', alt: 'caswino' },
+			title: 'Caswino2',
+			text: '600% UP TO 4800$ <br> +255 FREE SPINS',
+			btns: {
+				review: '',
+				play: '',
+			},
+		},
+	],
+	blackjack: [
+		{
+			img: { url: './img/partners/caswino.png', alt: 'caswino' },
+			title: 'Caswino3',
+			text: '600% UP TO 4800$ <br> +255 FREE SPINS',
+			btns: {
+				review: '',
+				play: '',
+			},
+		},
+		{
+			img: { url: './img/partners/caswino.png', alt: 'caswino' },
+			title: 'Caswino3',
+			text: '600% UP TO 4800$ <br> +255 FREE SPINS',
+			btns: {
+				review: '',
+				play: '',
+			},
+		},
+		{
+			img: { url: './img/partners/caswino.png', alt: 'caswino' },
+			title: 'Caswino3',
+			text: '600% UP TO 4800$ <br> +255 FREE SPINS',
+			btns: {
+				review: '',
+				play: '',
+			},
+		},
+		{
+			img: { url: './img/partners/caswino.png', alt: 'caswino' },
+			title: 'Caswino3',
+			text: '600% UP TO 4800$ <br> +255 FREE SPINS',
+			btns: {
+				review: '',
+				play: '',
+			},
+		},
+	],
+	poker: [
+		{
+			img: { url: './img/partners/caswino.png', alt: 'caswino' },
+			title: 'Caswino4',
+			text: '600% UP TO 4800$ <br> +255 FREE SPINS',
+			btns: {
+				review: '',
+				play: '',
+			},
+		},
+		{
+			img: { url: './img/partners/caswino.png', alt: 'caswino' },
+			title: 'Caswino4',
+			text: '600% UP TO 4800$ <br> +255 FREE SPINS',
+			btns: {
+				review: '',
+				play: '',
+			},
+		},
+		{
+			img: { url: './img/partners/caswino.png', alt: 'caswino' },
+			title: 'Caswino4',
+			text: '600% UP TO 4800$ <br> +255 FREE SPINS',
+			btns: {
+				review: '',
+				play: '',
+			},
+		},
+		{
+			img: { url: './img/partners/caswino.png', alt: 'caswino' },
+			title: 'Caswino4',
+			text: '600% UP TO 4800$ <br> +255 FREE SPINS',
+			btns: {
+				review: '',
+				play: '',
+			},
+		},
+	],
+	baccarat: [
+		{
+			img: { url: './img/partners/caswino.png', alt: 'caswino' },
+			title: 'Caswino5',
+			text: '600% UP TO 4800$ <br> +255 FREE SPINS',
+			btns: {
+				review: '',
+				play: '',
+			},
+		},
+		{
+			img: { url: './img/partners/caswino.png', alt: 'caswino' },
+			title: 'Caswino5',
+			text: '600% UP TO 4800$ <br> +255 FREE SPINS',
+			btns: {
+				review: '',
+				play: '',
+			},
+		},
+		{
+			img: { url: './img/partners/caswino.png', alt: 'caswino' },
+			title: 'Caswino5',
+			text: '600% UP TO 4800$ <br> +255 FREE SPINS',
+			btns: {
+				review: '',
+				play: '',
+			},
+		},
+		{
+			img: { url: './img/partners/caswino.png', alt: 'caswino' },
+			title: 'Caswino5',
+			text: '600% UP TO 4800$ <br> +255 FREE SPINS',
+			btns: {
+				review: '',
+				play: '',
+			},
+		},
+	],
+}
