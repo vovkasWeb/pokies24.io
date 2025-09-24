464e929639819a13c9465fa3777414077d84c304
Vova <103131693+vovkasWeb@users.noreply.github.com>
2025-09-24 21:13:48 +0300

    add functional popup

---
+++ js/clickBanner.js
@@ -0,0 +1,17 @@
+const header_btn = document.querySelector('.header__btn');
+const popup = document.querySelector('.header__popup');
+const popupCloseBtn = popup.querySelector('.header__popup-close-btn')
+header_btn.addEventListener('click', () => {
+    popup.classList.toggle('active');
+    header_btn.classList.toggle('clicked')
+});
+
+popupCloseBtn.addEventListener('click', () => {
+    popup.classList.remove('active');
+    header_btn.classList.remove('clicked');
+});
+popup.addEventListener('click', (e) => {
+   if(e.target.classList[0] === 'header__popup') {
+        window.location.replace('https://example.com')
+   }
+});
